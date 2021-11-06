56a48690003776b7939c0ff2b6903558be12c20a
Kura0913 <sakula112996532@gmail.com>
2021-11-07 01:27:20 +0800

    V1.0

---
+++ Toast_Kotlin/app/src/main/java/com/example/toast_kotlin/MainActivity.kt
@@ -0,0 +1,93 @@
+package com.example.toast_kotlin
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.Gravity
+import android.widget.Button
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
+import com.google.android.material.snackbar.Snackbar
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        //將變數與XML元件綁定
+        val btn_toast = findViewById<Button>(R.id.btn_toast)
+        val btn_custom = findViewById<Button>(R.id.btn_custom)
+        val btn_snackbar = findViewById<Button>(R.id.btn_snackbar)
+        val btn_dialog1 = findViewById<Button>(R.id.btn_Dialog1)
+        val btn_dialog2 = findViewById<Button>(R.id.btn_Dialog2)
+        val btn_dialog3 = findViewById<Button>(R.id.btn_Dialog3)
+        //建立要顯示在的列表上的字串
+        val item = arrayOf("選項1","選項2","選項3","選項4","選項5")
+        //Button點擊事件
+        btn_toast.setOnClickListener{
+            showToast("預設Toast")//執行showToast方法
+        }
+
+        btn_custom.setOnClickListener{
+            //宣告Toast
+            val toast =Toast(this)
+            //Toast在畫面中顯示位置
+            toast.setGravity(Gravity.TOP,0,50)
+            //Toast在畫面中顯示的持續時間
+            toast.duration = Toast.LENGTH_SHORT
+            //放入自訂義的畫面custom_toast.xml
+            toast.view = layoutInflater.inflate(R.layout.custom_toast,null)
+            //顯示於螢幕
+            toast.show()
+        }
+
+        btn_snackbar.setOnClickListener{
+            //建立Snackbar
+            Snackbar.make(it,"按鈕式Snackbar",Snackbar.LENGTH_SHORT).setAction("按鈕"){
+                showToast("已回應")
+            }.show()
+        }
+
+        btn_dialog1.setOnClickListener{
+            //建立AlertDialog物件
+            AlertDialog.Builder(this)
+                .setTitle("按鈕式AlertDialog")
+                .setMessage("AlertDialog內容")
+                .setNeutralButton("左按鈕"){ dialog, which ->
+                    showToast("左按鈕")
+                }
+                .setNegativeButton("中按鈕"){ dialog, which ->
+                    showToast("中按鈕")
+                }
+                .setPositiveButton("右按鈕"){ dialog, which ->
+                    showToast("右按鈕")
+                }.show()
+
+        }
+
+        btn_dialog2.setOnClickListener{
+            //建立AlertDialog物件
+            AlertDialog.Builder(this)
+                .setTitle("列表式AlertDialog")
+                .setItems(item){ dialogInterface,i ->
+                    showToast("你選的是${item[i]}")}.show()
+
+
+        }
+
+        btn_dialog3.setOnClickListener{
+            var position = 0
+            //建立AlertDialog物件
+            AlertDialog.Builder(this)
+                .setTitle("單選式AlertDialog")
+                .setSingleChoiceItems(item,0){ dialogInterface,i->
+                    position = i
+                }
+                .setPositiveButton("確定"){dialog, which ->
+                    showToast("你選的是${item[position]}")
+                }.show()
+        }
+
+
+    }
+    //建立showToast方法顯示Toast訊息
+    private fun showToast(msg:String)=Toast.makeText(this,msg,Toast.LENGTH_SHORT).show()
+}
